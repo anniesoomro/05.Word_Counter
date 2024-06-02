27be3dbf509e24729741003b862d83408b9e4522
anniesoomro <162474299+anniesoomro@users.noreply.github.com>
2024-06-02 16:24:28 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# 05.Word_Counter" 
---
+++ main.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+import chalk from "chalk"
+
+
+let user_ans = await inquirer.prompt({
+    type: "input",
+    name: "words",
+    message: "Please enter your sentence"
+})
+
+let word_count = user_ans.words.trim().split(" ").length
+
+console.log(chalk.blue.bold(`Your sentence has ${word_count}`));
